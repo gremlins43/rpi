0b14a036c3737ad75041d4d09286c0891a2ae0d3
gremlins43 <fast_pizza@hotmail.fr>
2019-12-26 22:52:35 +0100

    commit minmax

--- temperatura/minmaxTemp.sh
+++ temperatura/minmaxTemp.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# work on script directory
+cd "$(dirname "$0")"
+
 #get temperature from rpi
 TEMP=$(vcgencmd measure_temp)
 
@@ -7,37 +10,37 @@ TEMP=$(vcgencmd measure_temp)
 TEMP=$(echo $TEMP | grep -oP [0-9]{2})
 TODAY=$(echo "`date +%D`")
 
-if [ -f "/home/pi/script/temperatura/temp_log.csv" ]
+if [ -f temp_log.csv ]
 then
-	echo "`date +%D` `date +%T`;" $TEMP >> "/home/pi/script/temperatura/temp_log.csv"
+        echo "`date +%D` `date +%T`;" $TEMP >> temp_log.csv
 
-	if [ -f "/home/pi/script/temperatura/dayMinMax.csv" ]
-	then
-		if [ -f "/home/pi/script/temperatura/currentDay" ]
-		then
-			currentDay=$(cat /home/pi/script/temperatura/currentDay)
-				if [ "$currentDay" == "$TODAY" ]
-				then
-					if [ -f "/home/pi/script/temperatura/today.csv" ]
-					then
-						echo "`date +%D` `date +%T`;" $TEMP >> "/home/pi/script/temperatura/today.csv"
-					else
-						echo "`date +%D` `date +%T`;" $TEMP > "/home/pi/script/temperatura/today.csv"
-					fi
-				else
-					Maxi=$(cat today.csv | cut -c20-21 | sort | tail -n 1)
-					Mini=$(cat today.csv | cut -c20-21 | sort | head -n 1)
-					cat /home/pi/script/temperatura/today.csv | egrep "($Maxi)|($Mini)" >> "/home/pi/script/temperatura/dayMinMax.csv"
-					echo " $Maxi" "_and_" " $Mini"
-					echo "`date +%D` `date +%T`;" $TEMP > "/home/pi/script/temperatura/today.csv"
-					echo $TODAY > "/home/pi/script/temperatura/currentDay"
-				fi
-		else
-			echo $TODAY > "/home/pi/script/temperatura/currentDay"
-		fi
-	else
-		touch /home/pi/script/temperatura/dayMinMax.csv
-	fi
+        if [ -f dayMinMax.csv ]
+        then
+                if [ -f currentDay ]
+                then
+                        currentDay=$(cat /home/pi/script/temperatura/currentDay)
+                                if [ "$currentDay" == "$TODAY" ]
+                                then
+                                        if [ -f today.csv ]
+                                        then
+                                                echo "`date +%D` `date +%T`;" $TEMP >> today.csv
+                                        else
+                                                echo "`date +%D` `date +%T`;" $TEMP > today.csv
+                                        fi
+                                else
+                                        Maxi=$(cat today.csv | cut -c20-21 | sort | tail -n 1)
+                                        Mini=$(cat today.csv | cut -c20-21 | sort | head -n 1)
+                                        cat today.csv | egrep "( $Maxi)|( $Mini)" >> dayMinMax.csv
+                                        echo " $Maxi" "_and_" " $Mini"
+                                        echo "`date +%D` `date +%T`;" $TEMP > today.csv
+                                        echo $TODAY > currentDay
+                                fi
+                else
+                        echo $TODAY > currentDay
+                fi
+        else
+                touch /home/pi/script/temperatura/dayMinMax.csv
+        fi
 else
-		echo "`date +%D` `date +%T`;" $TEMP >> "/home/pi/script/temperatura/temp_log.csv"
+                echo "`date +%D` `date +%T`;" $TEMP >> temp_log.csv
 fi
